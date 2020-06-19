870453559287b7ed16c342a4347312adf38a673e
Detlaf <katya.gleh@yandex.ru>
2020-06-19 13:58:42 +0300

    User profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo,Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -25,3 +25,10 @@ class RegistrationForm(FlaskForm):
         user_email = User.query.filter_by(email=email.data).first()
         if user_email is not None:
             raise ValidationError('User with this email already exists!')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+    
