5ba03ea321d4d3bbdfd7524a4bcd1ba2017b66a1
Detlaf <katya.gleh@yandex.ru>
2020-06-28 21:32:18 +0300

    corrected avatar functionality

--- tests.py
+++ tests.py
@@ -22,8 +22,8 @@ class UserModelCase(unittest.TestCase):
     def test_avatar(self):
         u = User(username='john', email='john@example.com')
         self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
-                                         'd4c74594d841139328695756648b6bd6'
-                                         '?d=identicon&s=128'))
+                                        'd4c74594d841139328695756648b6bd6'
+                                        '?d=identicon&s=128'))
 
 
     def test_follow(self):
